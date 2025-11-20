57d075655feac5e23c507bcfd51f9e9bc894d8fd
ramak8sgcp <ramak8sgcp@gmail.com>
2025-11-20 20:40:48 +0530

    update

--- 30-rds/provider.tf
+++ 30-rds/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "81s-remote-state-rama"
-    key    = "expense-bastion"
+    key    = "expense-rds"
     region = "us-east-1"
     dynamodb_table = "81s-locking-dev-rama"
   }
