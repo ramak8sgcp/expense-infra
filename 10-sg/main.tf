db20d156f163c949374eba2f9f33c50350f68ab2
ramak8sgcp <ramak8sgcp@gmail.com>
2025-11-24 17:55:58 +0530

    update

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -48,93 +48,93 @@ module "ingress_alb_sg" {
     #sg_tags = var.node_sg_tags
 }
 
-# resource "aws_security_group_rule" "ingress_alb_https" {
-#   type              = "ingress"
-#   from_port         = 443
-#   to_port           = 443
-#   protocol          = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-#   security_group_id = module.ingress_alb_sg.id
-# }
+resource "aws_security_group_rule" "ingress_alb_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.ingress_alb_sg.id
+}
 
-# resource "aws_security_group_rule" "node_ingress_alb" {
-#   type              = "ingress"
-#   from_port         = 30000
-#   to_port           = 32767
-#   protocol          = "tcp"
-#   source_security_group_id = module.ingress_alb_sg.id
-#   security_group_id = module.node_sg.id
-# }
+resource "aws_security_group_rule" "node_ingress_alb" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32767
+  protocol          = "tcp"
+  source_security_group_id = module.ingress_alb_sg.id
+  security_group_id = module.node_sg.id
+}
 
-# resource "aws_security_group_rule" "node_eks_control_plane" {
-#   type              = "ingress"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   source_security_group_id = module.eks_control_plane_sg.id
-#   security_group_id = module.node_sg.id
-# }
+resource "aws_security_group_rule" "node_eks_control_plane" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  source_security_group_id = module.eks_control_plane_sg.id
+  security_group_id = module.node_sg.id
+}
 
-# resource "aws_security_group_rule" "eks_control_plane_node" {
-#   type              = "ingress"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   source_security_group_id = module.node_sg.id
-#   security_group_id = module.eks_control_plane_sg.id
-# }
+resource "aws_security_group_rule" "eks_control_plane_node" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  source_security_group_id = module.node_sg.id
+  security_group_id = module.eks_control_plane_sg.id
+}
 
-# resource "aws_security_group_rule" "eks_control_plane_bastion" {
-#   type              = "ingress"
-#   from_port         = 443
-#   to_port           = 443
-#   protocol          = "tcp"
-#   source_security_group_id = module.bastion_sg.id
-#   security_group_id = module.eks_control_plane_sg.id
-# }
+resource "aws_security_group_rule" "eks_control_plane_bastion" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  source_security_group_id = module.bastion_sg.id
+  security_group_id = module.eks_control_plane_sg.id
+}
 
-# resource "aws_security_group_rule" "node_vpc" {
-#   type              = "ingress"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   cidr_blocks = ["10.0.0.0/16"]
-#   security_group_id = module.node_sg.id
-# }
+resource "aws_security_group_rule" "node_vpc" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks = ["10.0.0.0/16"]
+  security_group_id = module.node_sg.id
+}
 
-# resource "aws_security_group_rule" "node_bastion" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   source_security_group_id = module.bastion_sg.id
-#   security_group_id = module.node_sg.id
-# }
+resource "aws_security_group_rule" "node_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.bastion_sg.id
+  security_group_id = module.node_sg.id
+}
 
-# resource "aws_security_group_rule" "mysql_bastion" {
-#   type              = "ingress"
-#   from_port         = 3306
-#   to_port           = 3306
-#   protocol          = "tcp"
-#   source_security_group_id       = module.bastion_sg.id
-#   security_group_id = module.mysql_sg.id
-# }
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id       = module.bastion_sg.id
+  security_group_id = module.mysql_sg.id
+}
 
 
-# resource "aws_security_group_rule" "bastion_public" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-#   security_group_id = module.bastion_sg.id
-# }
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.id
+}
 
-# resource "aws_security_group_rule" "mysql_node" {
-#   type              = "ingress"
-#   from_port         = 3306
-#   to_port           = 3306
-#   protocol          = "tcp"
-#   source_security_group_id = module.node_sg.id
-#   security_group_id = module.mysql_sg.id
-# }
+resource "aws_security_group_rule" "mysql_node" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.node_sg.id
+  security_group_id = module.mysql_sg.id
+}
